f654890af018353952a9de07e7f8ccedb5eda46e
gordan-radulovic <gr5345@student.uni-lj.si>
2022-01-10 15:38:52 +0100

    _

--- web/Program.cs
+++ web/Program.cs
@@ -6,7 +6,7 @@ using web.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("SchoolContext");
-var connectionString2 = builder.Configuration.GetConnectionString("AzureContext");
+var connectionString2 = builder.Configuration.GetConnectionString("PostContext");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
